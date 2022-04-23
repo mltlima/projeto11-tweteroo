7c372605f8c550cc2131226950da12934b79feca
mltlima <miguellt@dcc.ufrj.br>
2022-04-22 23:25:14 -0300

    sign-up e tweets

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 import express from 'express';
+import cors from 'cors';
 
-const app = express(); 
+const usernames =[];
+const tweets = [];
 
-app.get("/", (req, res) => {
-    res.send('Hello World');
+const app = express();
+app.use(express.json());
+app.use(cors()); 
+
+
+app.post("/sign-up", (req, res) => {
+    const body = req.body;
+    const username = {
+        username: body.username,
+        avatar: body.avatar
+    };
+    usernames.push(username);
+    res.send(username);
 });
+
+app.post("/tweets", (req, res) => {
+    const body = req.body;
+    const tweet = {
+        username: body.username,
+        tweet: body.tweet
+    };
+    tweets.push(tweet);
+    res.send(tweet);
+});
+
+app.get("/tweets", (req, res) => {
+    //get last ten items in tweets
+    const lastTenTweets = tweets.slice(-10);
+    res.send(lastTenTweets);
+});
+
 
 
 app.listen(5000);

1a18416bfa632aec83c4dc5b207611c960c5667f
mltlima <miguellt@dcc.ufrj.br>
2022-04-24 21:29:59 -0300

    retornar tweets do usuario

--- index.js
+++ index.js
@@ -11,28 +11,43 @@ app.use(cors());
 
 app.post("/sign-up", (req, res) => {
     const body = req.body;
-    const username = {
-        username: body.username,
-        avatar: body.avatar
-    };
-    usernames.push(username);
-    res.send(username);
+
+    if(body.username == "" || body.avatar == ""){
+        res.status(400).send("Todos os campos são obrigatórios!");
+    } else {
+        const username = {
+            username: body.username,
+            avatar: body.avatar
+        };
+        usernames.push(username);
+        res.status(201).send("OK");
+    }
 });
 
 app.post("/tweets", (req, res) => {
     const body = req.body;
-    const tweet = {
-        username: body.username,
-        tweet: body.tweet
-    };
-    tweets.push(tweet);
-    res.send(tweet);
+
+    if (body.username == "" || body.tweet == "") {
+        res.status(400).send("Todos os campos são obrigatórios!");
+    } else {
+        const tweet = {
+            username: body.username,
+            tweet: body.tweet
+        };
+        tweets.push(tweet);
+        res.status(201).send("OK");
+    }
 });
 
 app.get("/tweets", (req, res) => {
     //get last ten items in tweets
     const lastTenTweets = tweets.slice(-10);
     res.send(lastTenTweets);
+});
+
+app.get("/tweets/:username", (req, res) => {
+    const userTweets = tweets.filter(tweet => tweet.username == req.params.username);
+    res.send(userTweets);
 });
 
 
